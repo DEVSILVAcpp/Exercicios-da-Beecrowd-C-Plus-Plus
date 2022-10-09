2d2154618d340a893fa26d04fd125f2cbba6d20f
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-09 14:16:00 -0300

    Beecrowd | 1004 Produto Simples
    C++

---
+++ 1004.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Produto Simples
+
+using namespace std;
+
+int main(){
+
+    int A, B, SOMA;
+    cin >> A;
+    cin >> B;
+    SOMA = A * B;
+    cout << "PROD = " << SOMA << endl;
+
+    return 0;
+}
