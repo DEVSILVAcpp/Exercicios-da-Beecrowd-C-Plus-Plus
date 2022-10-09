23d162a867379e2b712009421e53a8a4eeee29bb
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-09 14:07:12 -0300

    Beecrowd | 1003 Soma Simples
    C++

---
+++ 1003.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Soma Simples
+
+using namespace std;
+
+int main(){
+
+    int A, B, SOMA;
+    cin >> A;
+    cin >> B;
+    SOMA = A + B;
+    cout << "SOMA = " << SOMA << endl;
+
+    return 0;
+}
