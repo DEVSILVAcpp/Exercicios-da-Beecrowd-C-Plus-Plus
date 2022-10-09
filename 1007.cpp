d6717e2a1f64d34a663c518b5b08d33d7fdcce7b
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-09 15:15:00 -0300

    Beecrowd | 1007 Diferença
    C++

---
+++ 1007.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Diferença
+
+using namespace std;
+
+int main(){
+
+    int A, B, C, D, DIFERENCA;
+    cin >> A >> B >> C >> D;
+    DIFERENCA = (A * B - C * D);
+    cout << "DIFERENCA = "<< DIFERENCA << endl;
+
+    return 0;
+}
