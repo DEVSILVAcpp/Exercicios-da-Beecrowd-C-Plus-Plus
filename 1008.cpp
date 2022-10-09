a5736f146e426cedc17d9482a5617f48d9d676d0
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-09 15:23:10 -0300

    Beecrowd | 1008 Salário
    C++

--- 1008.cpp
+++ 1008.cpp
@@ -3,3 +3,19 @@
 #include <string>
 #include <cmath>
 #include <stdlib.h>
+
+//Salário
+
+using namespace std;
+
+int main(){
+
+    double A, B, C, SALARY;
+    cin >> A >> B >> C;
+    cout << "NUMBER = " << A << endl;
+    cout << fixed << setprecision(2);
+    SALARY = B * C;
+    cout << "SALARY = U$ " << SALARY << endl;
+
+    return 0;
+}
