99c2f8fd0ba4a8329ad5c3cbac603703e21ab613
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-09 13:39:39 -0300

    Beecrowd | 1001 Extremamente Básico
    C++

---
+++ 1001.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Extremamente Básico
+
+using namespace std;
+
+int main(){
+
+    int A, B, SOMA;
+    cin >> A;
+    cin >> B;
+    SOMA = A + B;
+    cout << "X = " << SOMA << endl;
+
+    return 0;
+}
