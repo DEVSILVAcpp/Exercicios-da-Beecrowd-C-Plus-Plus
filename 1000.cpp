84ed3e2647184c68616b52c3b4177b6bab658fd8
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-09 13:26:07 -0300

    Beecrowd | 1000 Hello World!
    C++

---
+++ 1000.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Hello World!
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello World!" << endl;
+
+    return 0;
+
+}
