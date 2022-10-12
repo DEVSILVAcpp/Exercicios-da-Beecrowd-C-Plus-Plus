d09919404a0ddfb7edfa2ac01913c7cc9c918d11
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-12 20:25:58 -0300

    Beecrowd | 1013 O Maior
    C++

---
+++ 1013.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//O Maior
+
+using namespace std;
+
+int main(){
+
+    int a, b, c, MaiorAB, Maior;
+    cin >> a >> b >> c;
+    MaiorAB = (a + b +abs(a - b))/2;
+    Maior = (MaiorAB + c +abs(MaiorAB - c))/2;
+    cout << Maior << " eh o maior" << endl;
+
+return 0;
+}
