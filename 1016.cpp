de9dba1e93e130f7c5a159269412defdc36c1545
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-14 16:26:35 -0300

    Beecrowd | 1016 Distância
    C++

---
+++ 1016.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Distância
+
+using namespace std;
+
+int main(){
+
+    int KM, Distancia;
+    cin >> KM;
+    Distancia= KM*2;
+    cout << Distancia << " minutos" << endl;
+
+    return 0;
+}
