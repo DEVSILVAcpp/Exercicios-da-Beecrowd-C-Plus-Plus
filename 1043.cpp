4e1b0c8596b6127d51f40babb7de3f9ffb5088ad
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-27 09:15:36 -0300

    Beecrowd | 1043 Triângulo
    C++

---
+++ 1043.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Triângulo
+
+using namespace std;
+
+int main(){
+
+    double A, B, C;
+
+    cin >> A >> B >> C;
+
+    if(A < B + C && B < A + C && C < A + B){
+        cout << "Perimetro = " << setprecision(1) << fixed << A+B+C << endl;
+    }else{
+        cout << "Area = " << setprecision(1) << fixed << (A+B)/2 * C << endl;
+    }
+
+    return 0;
+}
