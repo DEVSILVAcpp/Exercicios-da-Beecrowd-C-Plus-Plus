52ae3b71a3e85e93b4ec3642335ade7fadf93f9b
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-10-27 08:48:02 -0300

    Beecrowd | 1042 Sort Simples
    C++

---
+++ 1042.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Sort Simples
+
+using namespace std;
+
+int main(){
+
+int a, b, c, A, B, C;
+cin >> A >> B >> C;
+a=A, b=B, c=C;
+
+if(b < a) swap(a, b);
+if(c < b){
+    swap(b, c);
+    if(b < a) swap(a, b);
+}
+
+cout << a << endl << b << endl << c << endl;
+cout << endl;
+cout << A << endl << B << endl << C << endl;
+
+return 0;
+}
