ace15179b924d0ed21a76dd85610d8047cc294c2
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-11-04 10:05:06 -0300

    Beecrowd | 1059 Números Pares
    C++

---
+++ 1059.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Números Pares
+
+using namespace std;
+
+int main(){
+
+int i;
+
+for(i = 1; i < 101; i++){
+    if(i%2 ==0){
+        cout << i << endl;
+    }
+}
+   
+    return 0;
+}
