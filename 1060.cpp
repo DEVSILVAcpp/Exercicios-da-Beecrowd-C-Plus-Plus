6bde6717475e1e6a0bbfdd50edbe3f30243963f7
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-11-05 07:45:34 -0300

    Create 1060.cpp

---
+++ 1060.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Números Positivos
+
+using namespace std;
+
+int main(){
+
+
+
+    return 0;
+}
