9c306912b8bd250894ebafdc1ef3e630f1450a6b
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-11-05 08:05:26 -0300

    Beecrowd | 1060 Números Positivos
    C++

--- 1060.cpp
+++ 1060.cpp
@@ -10,7 +10,19 @@ using namespace std;
 
 int main(){
 
+int i;
+int p;
+double x;
 
+p=0;
+
+for(i = 1; i < 7; i = i + 1){
+    cin >> x;
+    if(x > 0.0)
+    p = p + 1;
+}
+
+cout << p << " valores positivos" << endl;
 
     return 0;
 }
