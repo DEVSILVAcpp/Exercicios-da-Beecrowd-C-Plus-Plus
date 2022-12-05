9e157f2b14fbf763727c854887744b9aa25c9ddf
Josias Gabriel da Silva <josiasjgs2003@gmail.com>
2022-12-05 09:38:03 -0300

    Beecrowd | 1133 Resto da Divisão
    C++

---
+++ 1133.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <stdlib.h>
+
+//Resto da Divisão
+
+using namespace std;
+
+int main(){
+
+    int x;
+    int y;
+
+    cin >> x >> y;
+
+    if(x>y){
+        for(int i=y+1; i<x; i++){
+            if(i % 5 == 2 || i % 5 == 3){
+                cout << i << endl;
+            }
+        }
+    }
+
+    else{
+        for(int i=x+1; i<y; i++){
+            if(i % 5 == 2 || i % 5 == 3){
+                cout << i << endl;
+            }
+        }
+    }
+
+return 0;
+}
